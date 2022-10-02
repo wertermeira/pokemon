663a3e8a7d9e2becc6b47df4bf3da0df51e9c45c
Werter Meira <wertermeira@gmail.com>
2022-10-01 23:01:36 -0300

    test jwt_token

--- lib/jwt_token.rb
+++ lib/jwt_token.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 class JwtToken
-  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
-
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, SECRET_KEY)
+      JWT.encode(payload, Rails.application.secrets.secret_key_base.to_s)
     end
 
     def decode(token)
-      decoded = JWT.decode(token, SECRET_KEY)[0]
+      decoded = JWT.decode(token, Rails.application.secrets.secret_key_base.to_s)[0]
       HashWithIndifferentAccess.new decoded
     end
   end
---
+++ spec/lib/jwt_token_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe JwtToken do
+  let(:time) { 24.hours.from_now }
+  let(:user_id) { rand(1..20) }
+  let(:payload) { { user_id: user_id, exp: time.to_i } }
+  let(:secret_key_base) { Faker::Internet.password }
+  let(:token) { Faker::Internet.password }
+
+  before do
+    allow(Rails.application.secrets).to receive(:secret_key_base).and_return(secret_key_base)
+  end
+
+  describe '.encode' do
+    before do
+      allow(JWT).to receive(:encode).with(payload, secret_key_base).and_return(token)
+    end
+
+    it do
+      expect(described_class.encode({ user_id: user_id }, time)).to eq(token)
+    end
+  end
+
+  describe '.decode' do
+    before do
+      allow(JWT).to receive(:decode).with(token, secret_key_base).and_return([payload])
+    end
+
+    it do
+      expect(described_class.decode(token)).to eq(HashWithIndifferentAccess.new(payload))
+    end
+  end
+end
